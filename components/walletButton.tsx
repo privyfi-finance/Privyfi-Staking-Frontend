b945c36a13b1ff4d123b4e011a922042bf68e9c4
sainiavi <avisaini063@gmail.com>
2025-09-15 14:29:58 +0530

    minst update

--- components/walletButton.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { WalletMultiButton } from "@demox-labs/miden-wallet-adapter-reactui";
-import "../app/styles/wallet.css";
-
-export default function WalletButton() {
-  return <WalletMultiButton className="custom-wallet-btn" />;
-}
